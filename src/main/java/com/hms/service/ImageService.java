b889b86a11f1a822b4a480c0588c18fd62ba560c
tusharishere <tusharsinha1452@gmail.com>
2024-11-12 23:48:24 +0530

    Add findImagesByProperty

--- src/main/java/com/hms/service/ImageService.java
+++ src/main/java/com/hms/service/ImageService.java
@@ -1,4 +1,28 @@
 package com.hms.service;
 
+import com.hms.entity.Images;
+import com.hms.entity.Property;
+import com.hms.repository.ImagesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ImageService {
+
+    private ImagesRepository imagesRepository;
+
+    public ImageService(ImagesRepository imagesRepository) {
+        this.imagesRepository = imagesRepository;
+    }
+
+    public Images addImages(Images image) {
+        Images savedImages = imagesRepository.save(image);
+        return savedImages;
+    }
+
+    public List<Images> ImagesByProperty(Property property){
+        List<Images> images = imagesRepository.findByProperty(property);
+        return images;
+    }
 }
