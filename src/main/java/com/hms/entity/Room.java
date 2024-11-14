c772bb9a8d5fb0829e9ff7ff2410db9cff3ecd4e
tusharishere <tusharsinha1452@gmail.com>
2024-11-14 15:51:19 +0530

    Room Entity Added

--- src/main/java/com/hms/entity/Room.java
+++ src/main/java/com/hms/entity/Room.java
@@ -1,7 +1,30 @@
 package com.hms.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "room")
 public class Room {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "room_type", nullable = false)
+    private Integer roomType;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "available_Count", nullable = false)
+    private String availableCount;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+}
