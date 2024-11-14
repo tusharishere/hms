dcfb0fe6006fc4b2803e1664684893b353446ebe
tusharishere <tusharsinha1452@gmail.com>
2024-11-14 15:53:52 +0530

    Booking Repository Added

--- src/main/java/com/hms/repository/BookingsRepository.java
+++ src/main/java/com/hms/repository/BookingsRepository.java
@@ -2,6 +2,16 @@ package com.hms.repository;
 
 import com.hms.entity.Bookings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface BookingsRepository extends JpaRepository<Bookings, Long> {
+    @Query("SELECT b FROM Bookings b WHERE b.property.id = :propertyId AND " + "b.roomType = :roomType AND " + "((b.checkInDate <= :checkOutDate AND b.checkOutDate >= :checkInDate))")
+    List<Bookings> findBookingsByPropertyIdAndDateRange(@Param("propertyId") long propertyId,
+                                                        @Param("roomType") String roomType,
+                                                        @Param("checkInDate") LocalDate checkInDate,
+                                                        @Param("checkOutDate") LocalDate checkOutDate);
 }
