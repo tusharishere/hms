9a6e2c14d9387b8618099a9b5b20a1e1bf70a724
tusharishere <tusharsinha1452@gmail.com>
2024-11-19 16:03:38 +0530

    SMS Service integrated

---
+++ src/main/java/com/hms/utils/SmsService.java
@@ -0,0 +1,32 @@
+package com.hms.utils;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+
+    @Value("${twilio.phone.number}")
+    private String twilioPhoneNumber;
+
+    public void sendSms(String to, String messageBody) {
+        Twilio.init(accountSid, authToken);
+
+        Message message = Message.creator(
+                new PhoneNumber(to),
+                new PhoneNumber(twilioPhoneNumber),
+                messageBody
+        ).create();
+
+
+        System.out.println("SMS sent successfully. SID: " + message.getSid());
+    }
+}
